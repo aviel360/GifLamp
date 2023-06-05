1966087009154c9ed4c7ed824e8946d64dbab30e
aviel360 <aviel3600@gmail.com>
2023-06-05 17:03:36 +0300

    s

--- GifApp/GifApp/ViewModel/MatrixViewModel.cs
+++ GifApp/GifApp/ViewModel/MatrixViewModel.cs
@@ -14,12 +14,19 @@ using System.IO.Ports;
 namespace GifApp
 {
     public class MatrixViewModel : BaseViewModel
-    {
+    {   
+
+        #region Enums & Constants
+        public enum DisplaySettings { NONE = 0, IMAGE = 1, GIF = 2 };
+
+        #endregion
+
         #region Constructors
         public MatrixViewModel()
         {
             GetPorts();
             InitMatrix();
+            MatColorsCurrent = MatColors[0];
         }
         #endregion
 
@@ -28,6 +35,12 @@ namespace GifApp
         {
             get { return m_arrPorts; }
             set { SetProperty(ref m_arrPorts, value); }
+        }
+
+        public ObservableCollection<LedState> MatColorsCurrent
+        {
+            get { return m_matColorsCurrent; }
+            set { SetProperty(ref m_matColorsCurrent, value); }
         }
 
         public string PortCurrent
@@ -41,11 +54,22 @@ namespace GifApp
             get { return m_MatSizes; }
             set { SetProperty(ref m_MatSizes, value); }
         }
+        public List<string> MatFrame
+        {
+            get { return m_MatFrame; }
+            set { SetProperty(ref m_MatFrame, value); }
+        }
 
         public string MatSizeCurrent
         {
             get { return m_MatSizeCurrent; }
             set { SetProperty(ref m_MatSizeCurrent, value); }
+        }
+
+        public string MatFrameCurrent
+        {
+            get { return m_MatFrameCurrent; }
+            set { SetProperty(ref m_MatFrameCurrent, value); }
         }
 
         public List<string> AnimationSpeed
@@ -58,10 +82,16 @@ namespace GifApp
             get { return m_AnimationSpeedCurrent; }
             set { SetProperty(ref m_AnimationSpeedCurrent, value); }
         }
-        public ObservableCollection<LedState> MatColors
+        public ObservableCollection<ObservableCollection<LedState>> MatColors
         {
             get { return m_matColors; }
             set { SetProperty(ref m_matColors, value); }
+        }
+
+        public DisplaySettings DisplaySetting
+        {
+            get { return m_DisplaySettingCurrent; }
+            set { SetProperty(ref m_DisplaySettingCurrent, value); }   
         }
         #endregion
 
@@ -69,12 +99,13 @@ namespace GifApp
         #region Methods
         public void InitMatrix()
         {
-            MatColors = new ObservableCollection<LedState>();
-            for (int i = 0; i < 16; i++)
+            MatColors = new ObservableCollection<ObservableCollection<LedState>>();
+            MatColors.Add(new ObservableCollection<LedState>());
+            for (int i = 0; i < 32; i++)
             {
-                for (int j = 0; j < 16; j++)
+                for (int j = 0; j < 32; j++)
                 {
-                    MatColors.Add(new LedState());
+                    MatColors[0].Add(new LedState());
                 }
             }
         }
@@ -93,19 +124,34 @@ namespace GifApp
             for (int i = 0; i < 16*16; i++)
             {
                 System.Windows.Media.Color iColor = System.Windows.Media.Color.FromRgb((byte)((arrImage[i] >> 16) & 0xFF), (byte)((arrImage[i] >> 8) & 0xFF), (byte)(arrImage[i] & 0xFF));
-                MatColors[i].Color  = new SolidColorBrush(iColor);
+                MatColors[0][i].Color  = new SolidColorBrush(iColor);
             }
+        }
+
+        public string GetDisplayString()
+        {
+            string strDisplay = "bin";
+            switch(DisplaySetting)
+            {
+                case DisplaySettings.IMAGE: strDisplay = "jpg"; break;
+                case DisplaySettings.GIF: strDisplay = "gif"; break;
+            }
+            return strDisplay;
         }
         #endregion
 
         #region Members
         protected List<string> m_MatSizes = new List<string> { "16x16", "16x32", "32x16", "32x32" };
+        protected List<string> m_MatFrame = new List<string> { "0" };
+        protected string m_MatFrameCurrent = "0";
         protected string m_MatSizeCurrent = string.Empty;
         protected List<string> m_AnimationSpeed = new List<string> { "x0.5", "x1", "x1.5", "x2" };
         protected string m_AnimationSpeedCurrent = "x1";
         protected ObservableCollection<string> m_arrPorts = new ObservableCollection<string>();
         protected string m_PortCurrent = string.Empty;
-        protected ObservableCollection<LedState> m_matColors = new ObservableCollection<LedState>();
+        protected ObservableCollection<ObservableCollection<LedState>> m_matColors = new ObservableCollection<ObservableCollection<LedState>>();
+        protected ObservableCollection<LedState> m_matColorsCurrent = new ObservableCollection<LedState>();
+        protected DisplaySettings m_DisplaySettingCurrent = DisplaySettings.NONE;
         #endregion
     }
 }
